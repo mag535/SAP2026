b1bb2cea8de506a3fc7310587ee2487bb4fdd5e3
Melissa Gray <61853436+mag535@users.noreply.github.com>
2026-08-13 01:54:03 -0400

    Added full controller support
    Implemente functions to use controller buttons as an alternative
    to keyboard or keyboard & mouse.
    Setup UI navigation on pause menu, inventory, and notebook to be
    used with controller.

--- Carp-edium/Assets/Scripts/PlayerControls/PlayerMenu.cs
+++ Carp-edium/Assets/Scripts/PlayerControls/PlayerMenu.cs
@@ -1,13 +1,23 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 
 namespace Carp {
     public class PlayerMenu : MonoBehaviour
     {
+        [SerializeField]
+        private GameObject parentOfInventoryFocus;
+        [SerializeField]
+        private GameObject parentOfNotebookFocus;
+        [SerializeField]
+        private GameObject pauseFocus;
+
+        private PlayerInput playerInput;
+
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
-            
+            playerInput = GetComponent<PlayerInput>();
         }
 
         public void TogglePauseMenu(InputAction.CallbackContext context) {
@@ -16,6 +26,79 @@ namespace Carp {
                     PauseMenu.Instance.Resume();
                 } else {
                     PauseMenu.Instance.Pause();
+                    // Set focused game object
+                    EventSystem.current.SetSelectedGameObject(pauseFocus.gameObject);
+                }
+            }
+        }
+
+        public void OpenInventory(InputAction.CallbackContext context) {
+            if (context.canceled) {
+                EvtSystem.EventDispatcher.Raise<RequestOpenInventory>( new
+                    RequestOpenInventory { });
+                if (playerInput != null) {
+                    playerInput.SwitchCurrentActionMap("UI");
+                }
+            }
+        }
+        public void CloseInventory(InputAction.CallbackContext context) {
+            if (context.canceled) {
+                EvtSystem.EventDispatcher.Raise<RequestCloseInventory>( new
+                    RequestCloseInventory { });
+                if (playerInput != null) {
+                    playerInput.SwitchCurrentActionMap("Game");
+                }
+            }
+        }
+
+        public void OpenNotebook(InputAction.CallbackContext context) {
+            if (context.canceled) {
+                EvtSystem.EventDispatcher.Raise<RequestOpenNotebookPre>( new
+                    RequestOpenNotebookPre { });
+                if (playerInput != null) {
+                    playerInput.SwitchCurrentActionMap("UI");
+                }
+            }
+        }
+        public void CloseNotebook(InputAction.CallbackContext context) {
+            if (context.canceled) {
+                EventSystem.current.SetSelectedGameObject(null);
+                EvtSystem.EventDispatcher.Raise<RequestCloseNotebook>( new
+                    RequestCloseNotebook { });
+                if (playerInput != null) {
+                    playerInput.SwitchCurrentActionMap("Game");
+                }
+            }
+        }
+
+        public void PageBackwards(InputAction.CallbackContext context) {
+            if (context.canceled) {
+                EvtSystem.EventDispatcher.Raise<RequestPreviousPage>( new
+                        RequestPreviousPage {});
+            }
+        }
+        public void PageForward(InputAction.CallbackContext context) {
+            if (context.canceled) {
+                EvtSystem.EventDispatcher.Raise<RequestNextPage>( new
+                        RequestNextPage {});
+            }
+        }
+
+        public void SwitchToInventory(InputAction.CallbackContext context) {
+            if (context.canceled) {
+                // Set focused game object
+                foreach (Transform childTransform in parentOfInventoryFocus.transform) {
+                    EventSystem.current.SetSelectedGameObject(childTransform.gameObject);
+                    break;
+                }
+            }
+        }
+        public void SwitchToNotebook(InputAction.CallbackContext context) {
+            if (context.canceled) {
+                // Set focused game object
+                foreach (Transform childTransform in parentOfNotebookFocus.transform) {
+                    EventSystem.current.SetSelectedGameObject(childTransform.gameObject);
+                    break;
                 }
             }
         }
